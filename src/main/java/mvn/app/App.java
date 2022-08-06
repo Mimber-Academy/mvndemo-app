b3d2347a9902622815f6eead6e516089d4ebe2a5
zafar <zzafar270@gmail.com>
2022-08-06 12:47:44 +0000

    Added pom.xml file

---
+++ src/main/java/mvn/app/App.java
@@ -0,0 +1,13 @@
+package mvn.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
